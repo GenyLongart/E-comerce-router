e8bfcdff5b66870ea09d25e872c93c3ddb4d9f25
Genesis Longart <gnsslr@gmail.com>
2023-08-11 11:51:00 -0400

    Integrando router y componentes

---
+++ src/Layout.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Home from './components/shared/pages/Home';
+import Detail from './components/shared/pages/Detail';
+import Footer from './components/shared/Footer';
+import Navbar from './components/shared/Nabvar';
+import Contact from './components/shared/pages/Contact';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <>
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path='/detail' element={<Detail />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+
+      </BrowserRouter>
+      <Footer />
+
+    </>
+
+  )
+}
+
+export default Layout
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Layout from './Layout.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Layout />
+  </React.StrictMode>,
+)
